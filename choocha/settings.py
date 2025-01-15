18656f0341d5905c185473925e2f2faad974a738
fominyh_vv <fominihvv@gamil.com>
2025-01-15 12:37:12 +0300

    Подключение OAuth

--- choocha/settings.py
+++ choocha/settings.py
@@ -108,11 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
-
 TIME_ZONE = 'Europe/Moscow'
-
-USE_I18N = True
-
 USE_TZ = True
 
 
